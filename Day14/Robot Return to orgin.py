ceea744b31d6040c4950bdbc5ec56cd4960f2977
Abu Thwahir <abuthwahir2003@gmail.com>
2026-07-30 23:15:06 +0530

    Implement robot return to origin solution

---
+++ Day14/Robot Return to orgin.py
@@ -0,0 +1,16 @@
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        x=0
+        y=0
+
+        for ch in moves:
+            if ch=='U':
+                y+=1
+            elif ch=='D':
+                y-=1
+            elif ch=="R":
+                x+=1
+            else:
+                x-=1
+        return x==0 and y==0                    
+        
